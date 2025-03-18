6f5cb64d74b3028c945eaa2eed6d939451928447
Anuj <anujtiwari4454@outlook.com>
2025-03-18 18:08:50 +0530

    addchnages

--- Museum-Api/server.js
+++ Museum-Api/server.js
@@ -32,7 +32,7 @@ app.use((err, req, res, next) => {
 
 // For local development
 if (process.env.NODE_ENV !== 'production') {
-  const PORT = process.env.PORT || 5000;
+  const PORT = process.env.PORT || 9000;
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }
 
