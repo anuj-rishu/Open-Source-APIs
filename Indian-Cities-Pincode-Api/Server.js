be7dfceb5babc0162df50ea91f56001f1a351c51
Anuj <anujtiwari4454@outlook.com>
2025-06-21 20:24:24 +0530

    add indain pin code api

--- Indian-Cities-Pincode-Api/Server.js
+++ Indian-Cities-Pincode-Api/Server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const pincodeRoutes = require('./routes/pincodeRoutes');
+require('dotenv').config();
 
 const app = express();
 connectDB();
 
 app.use('/api', pincodeRoutes);
 
-app.listen(9000, () => {
-  console.log('Server running on port 9000');
+const PORT = process.env.PORT || 9000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
